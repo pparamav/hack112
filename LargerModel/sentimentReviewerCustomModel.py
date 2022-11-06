73f1996356cb8dbd13dbb1876288a27951f83ea1
Isaiah1810 <108897453+Isaiah1810@users.noreply.github.com>
2022-11-06 01:06:26 -0500

    A custom classifier model
    This model was trained with more parameters, but only 1 iteration

---
+++ LargerModel/sentimentReviewerCustomModel.py
@@ -0,0 +1,82 @@
+
+
+from urllib.request import urlopen
+import ssl
+from bs4 import BeautifulSoup
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+import pickle
+
+#Use conda environment
+#Pip install urlopen, bs4, nltk
+classifierF = open('naivebayesA.pickle', 'rb')
+classifier = pickle.load(classifierF)
+ssl._create_default_https_context = ssl._create_unverified_context
+def sentReview(movieName):
+    newName = ''
+
+    # make input url-friendly
+    for c in movieName:
+        if c == ' ':
+            newName += '_'
+        else:
+            newName += c
+    newName = newName.lower()
+
+
+
+    url = ("https://www.rottentomatoes.com/m/" + newName + 
+               "/reviews")
+    
+    # check url validity
+    try:
+        page = urlopen(url)
+    except:
+        print('Bad Url')
+        return
+            
+    html = page.read().decode("utf-8")
+
+    reviews = set()
+    firstLine = True
+    for review in html.split("""<div class="the_review" data-qa="review-text">"""):
+        if firstLine == True:
+            firstLine = False
+            continue
+
+        reviewEnd = review.find("</div>")
+        reviews.add(review[ : reviewEnd].strip())
+
+    posSum = 0
+    negSum = 0
+    neuSum = 0
+    sent = SentimentIntensityAnalyzer()
+    scores = []
+    for elem in reviews:
+        newElem = elem.split()
+        feats = dict([(word, True) for word in newElem])
+        newScore = classifier.classify(feats)
+        scores.append(newScore)
+    negScores = 0
+    posScores = 0
+    for elem in scores:
+        if elem == 'negative':
+            negScores += 1
+        elif elem == 'positive':
+            posScores += 1
+    if negScores >= posScores:
+        print('Bad Movie')
+    else:
+        print('Good Movie')
+    
+    
+
+def main():
+    print("Type movies to see their review sentiments. Press 'Q' to quit")
+    movieSearch = ''
+
+    while movieSearch != 'Q':
+        movieSearch = input("What movie do you want to search?")
+        sentReview(movieSearch)
+
+main()
